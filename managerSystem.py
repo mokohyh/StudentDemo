1ba77a6ad3fef623b4a0fe4306e426536575b076
mokohyh <1732918719@qq.com>
2019-08-03 11:21:38 +0800

    hyh3

---
+++ managerSystem.py
@@ -0,0 +1,10 @@
+class StudentManager():
+    def __init__(self):
+        self.student_list = []
+
+    def run(self):
+        while True:
+            # 显示功能菜单
+            # 用户输入目标功能序号
+            menu_num = int(input("please you need num"))
+            # 工具用户输入的序号执行不同的序号功能
