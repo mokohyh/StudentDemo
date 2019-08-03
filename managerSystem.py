c5f69fb7df3f9a460c35832c0322f82d4330d8b5
mokohyh <46769427+mokohyh@users.noreply.github.com>
2019-08-03 11:52:21 +0800

    Update managerSystem.py

--- managerSystem.py
+++ managerSystem.py
@@ -4,7 +4,7 @@ class StudentManager():
 
     def run(self):
         while True:
-            # 显示功能菜单
+            # 显示功能菜单11111
             # 用户输入目标功能序号
             menu_num = int(input("please you need num"))
             # 工具用户输入的序号执行不同的序号功能
